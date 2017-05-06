a5fc705c4947603832de09342a3fd21b30405749
andy <arueesch@gmail.com>
2017-05-06 16:46:29 +0200

    feat: Add collection view data source and delegate functionality to photoAlbumVC

---
+++ VirtualTourist/PhotoAlbumCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoAlbumCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Andreas Rueesch on 06.05.17.
+//  Copyright © 2017 Andreas Rueesch. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- VirtualTourist/PhotoAlbumViewController.swift
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoAlbumViewController: UIViewController {
     
+    // MARK: Members
+    
     var pin: Pin!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    
+    // MARK: Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +28,11 @@ class PhotoAlbumViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("photo album for \(pin) loaded")
+    
+        addLocationPinAndCenter(atLat: pin.latitude, atLong: pin.longitude)
         
+        // set layout of collection view cells
+        setFlowLayout()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,15 +40,98 @@ class PhotoAlbumViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // reload data
+        photoCollectionView.reloadData()
     }
-    */
+}
 
+// MARK: Collection View data source
+
+extension PhotoAlbumViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // TODO count
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoAlbumCell", for: indexPath) as! PhotoAlbumCollectionViewCell
+        
+        // TODO placeholder
+        cell.imageView.image = UIImage(named: "placeholderImg")
+        return cell
+    }
+    
+    
+    // MARK: Collection View Helper
+    
+    func setFlowLayout() {
+        let space: CGFloat = 3.0
+        let dimension: CGFloat
+        
+        if UIDeviceOrientationIsPortrait(UIDevice.current.orientation) {
+            dimension = (view.frame.size.width - (2 * space)) / 3.0
+        } else {
+            dimension = (view.frame.size.width - (4 * space)) / 5.0
+        }
+        
+        flowLayout.minimumLineSpacing = space
+        flowLayout.minimumInteritemSpacing = space
+        
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+    }
+}
+
+extension PhotoAlbumViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //let photo = pin.Photos[(indexPath as NSIndexPath).row]
+        
+        print("TODO delete photo at: \((indexPath as NSIndexPath).row)")
+    }
+    
+}
+
+
+
+// MARK: Map Kit delegats methods
+
+extension PhotoAlbumViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reusePinId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reusePinId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reusePinId)
+            pinView!.canShowCallout = false
+            pinView!.pinTintColor = .red
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func addLocationPinAndCenter(atLat lat: Double, atLong long: Double) {
+        
+        // add a pin annotation
+        let coordinates = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinates
+        // TODO decide if title and subtitle wanted
+        
+        mapView.addAnnotation(annotation)
+        
+        // set region
+        let regionRadius: CLLocationDistance = 2000         // in meters
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinates, regionRadius, regionRadius)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
